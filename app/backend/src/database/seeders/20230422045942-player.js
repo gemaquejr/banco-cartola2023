fa4a4e99ff9e8cee7b4afc92cd87689889ec14fb
Francisco Espíndola Gemaque Junior <gemaquejr@hotmail.com>
2023-04-22 02:20:54 -0300

    Adiciona seedMock de player

--- app/backend/src/database/seeders/20230422045942-player.js
+++ app/backend/src/database/seeders/20230422045942-player.js
@@ -1,24 +1,27 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  up: async (queryInterface, _Sequelize) => {
+    const players = [
+      {
+        name: 'Jogador 1',
+        age: 25,
+        club: 'Remo',
+        position: Atacante,
+        starter: true,
+        value: 100.50,
+        lastScore: 19.50,
+        minValueToIncrease: 2.30,
+        soccerMatch: 'Time A x Time B',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+    ];
+
+    await queryInterface.bulkInsert('Players', players, {});
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.bulkDelete('Players', null, {});
   }
 };
