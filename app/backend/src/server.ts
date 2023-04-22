40b0545a3dcdd0563ee97303a926964b4ae711d4
Francisco Espíndola Gemaque Junior <gemaquejr@hotmail.com>
2023-04-22 20:44:22 -0300

    Adiciona route em app

--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+import 'dotenv/config';
+import routes from './routes';
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(express.json());
+
+app.use('/teams', routes.teamRouter);
 
 app.listen(PORT, ()=> {
     console.log(`Server is running on port: ${PORT}`)
