c893ff9f813898e7604b15c2c1364042e799706b
Francisco Espíndola Gemaque Junior <gemaquejr@hotmail.com>
2023-04-24 14:48:33 -0300

    Adiciona Page TeamPage.jsx

---
+++ app/frontend/src/pages/Team.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TeamForm from "../components/TeamForm";
+
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Times</h1>
+      <TeamForm />
+    </div>
+  );
+};
+
+export default TeamPage;
