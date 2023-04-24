874cc6e06b752791f96e3f03274ffa745369ebc9
Francisco Espíndola Gemaque Junior <gemaquejr@hotmail.com>
2023-04-24 15:18:52 -0300

    Adiciona configuracao de header do cors no backend

--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import 'dotenv/config';
 import routes from './routes';
+import cors from 'cors';
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.json());
+
+app.use((_req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+})
 
 app.use('/teams', routes.teamRouter);
 app.use('/players', routes.playerRouter);
