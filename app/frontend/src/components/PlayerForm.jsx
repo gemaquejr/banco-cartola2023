a379ed49a8aded4b2968068949a817ead519f923
Francisco Espíndola Gemaque Junior <gemaquejr@hotmail.com>
2023-04-25 20:26:12 -0300

    Fix PlayerForm.jsx

--- app/frontend/src/components/PlayerForm.jsx
+++ app/frontend/src/components/PlayerForm.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { requestData, postData, putData, deleteData } from '../services/requests';
 
 const PlayerForm = () => {
-  const [player, setPlayer] = useState({ name: "", age: "", club: "", position: "", starter: false, value: "", last_score: "", min_value_to_increase: "", soccer_match: "" });
+  const [player, setPlayer] = useState({ name: "", age: "", club: "", position: "", starter: false });
   const [players, setPlayers] = useState([]);
   const [editing, setEditing] = useState(false);
   const [playerId, setPlayerId] = useState(null);
@@ -40,7 +40,7 @@ const PlayerForm = () => {
         await postData("/players", player);
       }
       fetchPlayers();
-      setPlayer({ name: "", age: "", club: "", position: "", starter: false, value: "", last_score: "", min_value_to_increase: "", soccer_match: "" });
+      setPlayer({ name: "", age: "", club: "", position: "", starter: false });
       setEditing(false);
       setPlayerId(null);
     } catch (error) {
@@ -51,7 +51,7 @@ const PlayerForm = () => {
   const handleEdit = (player) => {
     setPlayerId(player.id);
     setEditing(true);
-    setPlayer({ name: player.name, age: player.age, club: player.club, position: player.position, starter: player.starter, value: "", last_score: player.last_score, min_value_to_increase: player.min_value_to_increase, soccer_match: player.soccer_match });
+    setPlayer({ name: player.name, age: player.age, club: player.club, position: player.position, starter: player.starter });
   };
 
   const handleDelete = async (id) => {
@@ -104,40 +104,12 @@ const PlayerForm = () => {
             onChange={handleCheckboxChange}
             />
         </label>
-        <input
-          type="number"
-          name="value"
-          value={player.value}
-          onChange={handleChange}
-          placeholder="Valor do jogador"
-        />
-        <input
-          type="number"
-          name="last_score"
-          value={player.last_score}
-          onChange={handleChange}
-          placeholder="Última Pontuação"
-        />
-        <input
-          type="number"
-          name="min_value_to_increase"
-          value={player.min_value_to_increase}
-          onChange={handleChange}
-          placeholder="Mínimo pra valorizar"
-        />
-        <input
-          type="text"
-          name="soccer_match"
-          value={player.soccer_match}
-          onChange={handleChange}
-          placeholder="Partida"
-        />
         <button type="submit">{editing ? "Editar" : "Adicionar"}</button>
       </form>
       <ul>
         {players.map((player) => (
           <li key={player.id}>
-            {player.name} - {player.age} - {player.club} - {player.position} - {player.starter ? "Sim" : "Não"} - {player.value} - {player.last_score} - {player.min_value_to_increase} - {player.soccer_match}
+            {player.name} - {player.age} - {player.club} - {player.position} - {player.starter ? "Sim" : "Não"}
             <button onClick={() => handleEdit(player)}>Editar</button>
             <button onClick={() => handleDelete(player.id)}>Excluir</button>
           </li>
