1fb891aea9ed29e32811096af51cca780a6b7ed0
Francisco Espíndola Gemaque Junior <gemaquejr@hotmail.com>
2023-04-26 10:12:08 -0300

    Adiciona component Button com module.css

---
+++ app/frontend/src/components/Button/Button.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Button.module.css';
+
+function Button(props) {
+  const { onClick, label } = props;
+
+  return (
+    <button className={styles.c_button} type="button" onClick={onClick}>
+      {label}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired,
+};
+
+export default Button;
