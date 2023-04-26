aab0ea1958926c1e133d5577518277466417fb86
Francisco Espíndola Gemaque Junior <gemaquejr@hotmail.com>
2023-04-26 09:43:55 -0300

    Instala react-router-dom e configura rotas em app.js e index.js

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,9 +1,21 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
 import React from 'react';
-import TeamForm from './pages/Team';
+import Team from './pages/Team';
+import Coach from './pages/Coach';
+import Match from './pages/Match';
+import Player from './pages/Player';
+import Round from './pages/Round';
 
 function App() {
   return (
-    <TeamForm />
+    <Routes>
+      <Route path="/coaches" element={ <Coach /> } />
+      <Route path="/matches" element={ <Match /> } />
+      <Route path="/players" element={ <Player /> } />
+      <Route path="/rounds" element={ <Round /> } />
+      <Route path="/teams" element={ <Team /> } />
+      <Route exact path="/" element={ <Navigate to="/matches" /> } />
+    </Routes>
   );
 }
 
