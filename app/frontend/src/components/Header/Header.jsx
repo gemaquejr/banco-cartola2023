4b8977bc281a6783d4b19005c53205679d0fdacc
Francisco Espíndola Gemaque Junior <gemaquejr@hotmail.com>
2023-05-01 20:31:55 -0300

    Refatora Header.jsx

--- app/frontend/src/components/Header/Header.jsx
+++ app/frontend/src/components/Header/Header.jsx
@@ -1,37 +1,25 @@
-import React, { useState } from 'react';
-import Menu from './menu.png';
-import Close from './close.png';
+import React from 'react';
 import styles from './Header.module.css';
 
 function Header() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  }
-
   return (
     <header>
-      <div className={styles.container_navbar}>
-        <ul className={styles.container_links}>
-          <li><a href="hero">Home</a></li>
-          <li><a href="about">About</a></li>
-          <li><a href="services">Services</a></li>
-          <li><a href="contacts">contacts</a></li>
+      <nav>
+        <input className={styles.nav_toggle} id="nav-toggle" type="checkbox" />
+        <div className={styles.logo}><strong>Cartola 2023</strong></div>
+        <ul className={styles.links}>
+          <li><a href="teams">Times</a></li>
+          <li><a href="players">Jogadores</a></li>
+          <li><a href="coaches">Técnicos</a></li>
+          <li><a href="rounds">Rodada</a></li>
+          <li><a href="matches">Confrontos</a></li>
         </ul>
-        <a href="_blank" className={styles.container_action_btn}>Get Started</a>
-        <div className={styles.container_toggle_btn} onClick={toggleDropdown}>
-          <img src={isOpen ? Menu : Close} alt={isOpen ? "Header_Menu" : "Header_Close"} />
-        </div>
-      </div>
-
-      <div className={`${styles.container_dropdown_menu} ${isOpen ? "open" : ""}`}>
-        <li><a href="hero">Home</a></li>
-        <li><a href="about">About</a></li>
-        <li><a href="services">Services</a></li>
-        <li><a href="contacts">contacts</a></li>
-        <li><a href="_blank" className={styles.container_action_btn}>Get Started</a></li>
-      </div>
+        <label htmlFor="nav-toggle" className={styles['icon-burger']}>
+          <div className={styles.line}></div>
+          <div className={styles.line}></div>
+          <div className={styles.line}></div>
+        </label>
+      </nav>
     </header>
   );
 }
