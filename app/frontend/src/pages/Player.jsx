300fc8362c84978dfaa937fa4d321446504af22a
Francisco Espíndola Gemaque Junior <gemaquejr@hotmail.com>
2023-05-03 21:14:03 -0300

    Feat path de PlayerForm

--- app/frontend/src/pages/Player.jsx
+++ app/frontend/src/pages/Player.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import PlayerForm from "../components/Player/PlayerForm";
+import PlayerForm from "../components/Player/PlayerView";
 import Header from "../components/Header/Header";
 
 const PlayerPage = () => {
