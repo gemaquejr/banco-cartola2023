515e1cd12a902f386fe7b830c425274eaf25cad0
Francisco Espíndola Gemaque Junior <gemaquejr@hotmail.com>
2023-05-07 23:21:54 -0300

    fazendo relacao match/round em models

--- app/backend/src/database/models/match.ts
+++ app/backend/src/database/models/match.ts
@@ -1,6 +1,8 @@
 import { DataTypes, Model } from 'sequelize';
 import db from '.';
 
+import Round from './round';
+
 class Match extends Model {
   public id!: number;
   public homeTeam!: string;
@@ -9,6 +11,7 @@ class Match extends Model {
   public awayScore!: number;
   public date!: string;
   public stadium!: string;
+  public roundId!: number;
 
   public readonly createdAt!: Date;
   public readonly updatedAt!: Date;
@@ -43,7 +46,15 @@ Match.init({
     stadium: {
       type: DataTypes.STRING(100),
       allowNull: false,
-    }, 
+    },
+    round_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'rounds',
+        key: 'id'
+      }
+    }
 }, {
 underscored: true,
 sequelize: db,
@@ -51,4 +62,4 @@ modelName: 'matches',
 timestamps: false,
 });
 
-export default Match;
+Match.belongsTo(Round, { foreignKey: 'round_id' });
