fa0c02bd426cc1f3860a928ef4e78e396c17f33f
Francisco Espíndola Gemaque Junior <gemaquejr@hotmail.com>
2023-05-09 23:20:57 -0300

    Relaciona match e round em matchService.ts

--- app/backend/src/services/matchService.ts
+++ app/backend/src/services/matchService.ts
@@ -1,38 +1,57 @@
+import { Op } from 'sequelize';
 import Match from '../database/models/match';
+import Round from '../database/models/round';
 
 export default class MatchService {
-    public matchModel = Match;
+  public matchModel = Match;
 
-    public async createMatch(matchData: any) {
-        const match = await this.matchModel.create(matchData)
-        return match
-    }
+  public async createMatch(matchData: any) {
+    const match = await this.matchModel.create(matchData)
+    return match
+  }
 
-    public async getAllMatches() {
-        const matches = await this.matchModel.findAll();
-        return matches;
-    }
+  public async getAllMatches() {
+    const matches = await this.matchModel.findAll({
+      include: [Round],
+      order: [['date', 'ASC']],
+    });
+    return matches;
+  }
 
-    public async getMatchById(matchId: number) {
-        const match = await this.matchModel.findByPk(matchId);
-        return match
-    }
+  public async getMatchById(matchId: number) {
+    const match = await this.matchModel.findByPk(matchId, {
+      include: [Round],
+    });
+    return match
+  }
 
-    public async updateMatchById(matchId: number, matchData: any) {
-        const match = await this.matchModel.findByPk(matchId);
-        if (!match) {
-          throw new Error('Match not found');
-        }
-        await match.update(matchData);
-        return match;
-    }
+  public async getMatchesByRound(roundId: number) {
+    const matches = await this.matchModel.findAll({
+      where: {
+        roundId: {
+          [Op.eq]: roundId,
+        },
+      },
+      include: [Round],
+    });
+    return matches;
+  }
 
-    public async deleteMatchById(matchId: number) {
-        const match = await this.matchModel.findByPk(matchId);
-        if (!match) {
-          throw new Error('Match not found');
-        }
-        await match.destroy();
-        return match;
+  public async updateMatchById(matchId: number, matchData: any) {
+    const match = await this.matchModel.findByPk(matchId);
+    if (!match) {
+      throw new Error('Match not found');
     }
-}
+    await match.update(matchData);
+    return match;
+  }
+
+  public async deleteMatchById(matchId: number) {
+    const match = await this.matchModel.findByPk(matchId);
+    if (!match) {
+      throw new Error('Match not found');
+    }
+    await match.destroy();
+    return match;
+  }
+};
