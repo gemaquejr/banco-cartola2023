46cd72e9b0455277a449dd7e78f1e33e8d9caa7e
Francisco Espíndola Gemaque Junior <gemaquejr@hotmail.com>
2023-05-12 21:09:38 -0300

    Feat migration de team

--- app/backend/src/database/migrations/20230422211600-create-team.js
+++ app/backend/src/database/migrations/20230422211600-create-team.js
@@ -13,6 +13,10 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
+      logo_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
       createdAt: {
         type: Sequelize.DATE,
         allowNull: false,
