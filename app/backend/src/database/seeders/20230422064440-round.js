9682b3e0c6b31eb721e28d7d12f020e8572ef247
Francisco Espíndola Gemaque Junior <gemaquejr@hotmail.com>
2023-05-13 23:56:12 -0300

    Feat seed de round

--- app/backend/src/database/seeders/20230422064440-round.js
+++ app/backend/src/database/seeders/20230422064440-round.js
@@ -9,6 +9,60 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date()
       },
+      {
+        number: 2,
+        name: "Segunda Rodada",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        number: 3,
+        name: "Terceira Rodada",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        number: 4,
+        name: "Quarta Rodada",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        number: 5,
+        name: "Quinta Rodada",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        number: 6,
+        name: "Sexta Rodada",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        number: 7,
+        name: "Sétima Rodada",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        number: 8,
+        name: "Oitava Rodada",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        number: 9,
+        name: "Nona Rodada",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        number: 10,
+        name: "Nona Rodada",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
     ];
 
     await queryInterface.bulkInsert('rounds', rounds, {});
