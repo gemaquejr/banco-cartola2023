0ca1305ced10f5e411e3a89f6e813fac087c705a
Francisco Espíndola Gemaque Junior <gemaquejr@hotmail.com>
2023-05-13 23:54:27 -0300

    Feat seed de player

--- app/backend/src/database/seeders/20230422045942-player.js
+++ app/backend/src/database/seeders/20230422045942-player.js
@@ -11,9 +11,99 @@ module.exports = {
 
     const players = [
       {
-        name: 'Jogador 1',
+        name: 'Pedro Guilherme Abreu dos Santos',
         age: 25,
         position: 'Atacante',
+        starter: true,
+        team_id: team,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Gabriel Barbosa',
+        age: 26,
+        position: 'Atacante',
+        starter: true,
+        team_id: team,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Giorgian Daniel De Arrascaeta Benedetti',
+        age: 28,
+        position: 'Meio campo',
+        starter: true,
+        team_id: team,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Éverton Augusto de Barros Ribeiro',
+        age: 34,
+        position: 'Meio campo',
+        starter: true,
+        team_id: team,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Gerson Santos da Silva',
+        age: 25,
+        position: 'Meio campo',
+        starter: true,
+        team_id: team,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Thiago Maia Alencar',
+        age: 26,
+        position: 'Meio campo',
+        starter: true,
+        team_id: team,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Ayrton Lucas Dantas de Medeiros',
+        age: 25,
+        position: 'Lateral',
+        starter: true,
+        team_id: team,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Wesley Vinicius França Lima',
+        age: 25,
+        position: 'Lateral',
+        starter: true,
+        team_id: team,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Leonardo Pereira',
+        age: 27,
+        position: 'Zagueiro',
+        starter: true,
+        team_id: team,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Fabrício Bruno Soares de Faria',
+        age: 27,
+        position: 'Zagueiro',
+        starter: true,
+        team_id: team,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Matheus Cunha Queiroz',
+        age: 21,
+        position: 'Goleiro',
         starter: true,
         team_id: team,
         createdAt: new Date(),
