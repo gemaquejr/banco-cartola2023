26f0a5a5f9729cb9db2e7322964f8ae804955299
Francisco Espíndola Gemaque Junior <gemaquejr@hotmail.com>
2023-05-13 23:55:49 -0300

    Feat seed de match

--- app/backend/src/database/seeders/20230422060623-match.js
+++ app/backend/src/database/seeders/20230422060623-match.js
@@ -11,12 +11,45 @@ module.exports = {
 
     const matches = [
       {
-        home_team: 'Remo',
-        away_team: 'Paysandu',
+        home_team: 'Flamengo',
+        away_team: 'Fluminense',
         home_score: 4,
         away_score: 0,
         date: '22-04-2023',
-        stadium: 'Mangueirão',
+        stadium: 'Maracanã',
+        round_id: round,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        home_team: 'Grêmio',
+        away_team: 'Internacional',
+        home_score: 1,
+        away_score: 0,
+        date: '22-04-2023',
+        stadium: 'Maracanã',
+        round_id: round,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        home_team: 'Corinthians',
+        away_team: 'Santos',
+        home_score: 2,
+        away_score: 0,
+        date: '22-04-2023',
+        stadium: 'Maracanã',
+        round_id: round,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        home_team: 'Atlético-MG',
+        away_team: 'Cruzeiro',
+        home_score: 2,
+        away_score: 1,
+        date: '22-04-2023',
+        stadium: 'Maracanã',
         round_id: round,
         createdAt: new Date(),
         updatedAt: new Date()
