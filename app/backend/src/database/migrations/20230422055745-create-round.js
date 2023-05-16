6e1f8df0a2e06a280dbd6243e04e139b09447cf8
Francisco Espíndola Gemaque Junior <gemaquejr@hotmail.com>
2023-05-16 19:05:25 -0300

    Fix migration de create-round.js

---
+++ app/backend/src/database/migrations/20230422055745-create-round.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('rounds', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      number: {
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        onUpdate: Sequelize.literal('CURRENT_TIMESTAMP'),
+      }
+    });
+  },
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('rounds');
+  }
+};
