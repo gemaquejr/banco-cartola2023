c350327705f579a5ca1c4a0cb8b5fd25b297e0ec
Francisco Espíndola Gemaque Junior <gemaquejr@hotmail.com>
2023-05-15 23:35:59 -0300

    Adiciona model de stadium

--- app/backend/src/database/models/stadium.ts
+++ app/backend/src/database/models/stadium.ts
@@ -1,9 +1,12 @@
 import { DataTypes, Model } from 'sequelize';
 import db from '.';
 
+import Team from './team';
+
 class Stadium extends Model {
   public id!: number;
   public name!: string;
+  public teamId!: number;
 
   public readonly createdAt!: Date;
   public readonly updatedAt!: Date;
@@ -19,11 +22,17 @@ Stadium.init({
     type: DataTypes.STRING,
     allowNull: false,
   },
+  team_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  }
 }, {
   underscored: true,
   sequelize: db,
   modelName: 'stadiums',
   timestamps: false,
 });
+
+Stadium.belongsTo(Team, { foreignKey: 'team_id' });
 
 export default Stadium;
