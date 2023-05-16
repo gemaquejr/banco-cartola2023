cd455349b363e000dd237180a189000143014b26
Francisco Espíndola Gemaque Junior <gemaquejr@hotmail.com>
2023-05-16 18:49:36 -0300

    Fix migration de create-team.js

---
+++ app/backend/src/database/migrations/20230422053258-create-team.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('teams', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      team_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      logo_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        onUpdate: Sequelize.literal('CURRENT_TIMESTAMP'),
+      }
+    });  
+  },
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('teams');
+  }
+};
