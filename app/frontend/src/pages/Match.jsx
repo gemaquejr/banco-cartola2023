8c44ccf5101e14da5f2ddaf9f1513ea40a1a8cde
Francisco Espíndola Gemaque Junior <gemaquejr@hotmail.com>
2023-05-20 00:33:38 -0300

    Importa matchview.jsx e container_match

--- app/frontend/src/pages/Match.jsx
+++ app/frontend/src/pages/Match.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import MatchForm from "../components/Match/MatchForm";
+import MatchForm from "../components/Match/MatchView";
 import Header from "../components/Header/Header";
 
 const MatchPage = () => {
   return (
-    <div>
+    <div className="container_match">
       <Header />
       <MatchForm />
     </div>
